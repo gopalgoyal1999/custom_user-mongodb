d6c4e3f5d8aa5c07f6485745b7cbebcfe1c3a5ee
gopal  goyal <44091979+gopalgoyal1999@users.noreply.github.com>
2020-06-11 22:54:08 +0530

    Update views.py

--- login/views.py
+++ login/views.py
@@ -46,6 +46,16 @@ class UserLogin(GenericAPIView):
         except User.DoesNotExist:
             result = {'error': True, 'message': 'Invalid credentials'}
             return Response(data=result,status=status.HTTP_401_UNAUTHORIZED)
+
+class UserLogout(APIView):
+    permission_classes = (IsAuthenticated,)
+    def post(self,request):
+        logout(request)
+        response = redirect('/logoutapi/')
+        response.delete_cookie('loginapi')
+        return response
+
+
         # try:
         #     instance = User.objects.get(username=username)
         #     print(instance)
@@ -56,37 +66,3 @@ class UserLogin(GenericAPIView):
         #     # request.session.set_expiry(3600000)
         #     return Response(serializer.validated_data(data), status=status.HTTP_200_OK)
         # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-class UserLogout(APIView):
-    permission_classes = (IsAuthenticated,)
-    def post(self,request):
-        logout(request)
-        response = redirect('/logoutapi/')
-        response.delete_cookie('loginapi')
-        return response
-
-    #
-    # def post(self,request, *args, **kwargs):
-    #     data = request.data
-    #     username = request.data.get('username')
-    #     email = request.data.get('email')
-    #     password = request.data.get('password')
-    #     try:
-    #         user = User.objects.get(email=email)
-    #         if password == user['password']:
-    #             user.backend = 'django_mongoengine.mongo_auth.backends.MongoEngineBackend'
-    #             login(request, user,backend=user.backend)
-    #             try:
-    #                 instance = User.objects.get(username=username)
-    #             except User.DoesNotExist:
-    #                 instance = None
-    #             serializer = UserLoginSerializer(instance, data=data)
-    #             if serializer.is_valid():
-    #                 data = serializer.validated_data(data)
-    #                 result = {'message': 'you are logged In'}
-    #                 return Response(data=result, status=status.HTTP_200_OK)
-    #             result = {'error': True, 'message': 'Invalid credentials'}
-    #             return Response(data=result,status=status.HTTP_404_NOT_FOUND)
-    #     except User.DoesNotExist:
-    #         result = {'error': True, 'message': 'Invalid credentials'}
-    #         return Response(data=result,status=status.HTTP_401_UNAUTHORIZED)
